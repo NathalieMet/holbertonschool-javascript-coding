c747c97aca637e8d6fbca9634b557bb20cc3e3a0
NathalieMet <nathalie.metreau@hotmail.fr>
2024-01-22 11:02:37 +0100

    correct t0

--- 0-readme.js
+++ 0-readme.js
@@ -1,7 +1,17 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[2], (err, inputD) => {
-  if (err) throw err;
-  console.log(inputD.toString());
+const filePath = process.argv[2];
+
+if (!filePath) {
+  console.error('Usage: node script.js <file-path>');
+  process.exit(1);
+}
+
+fs.readFile(filePath, 'utf-8', (err, content) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(content);
+  }
 });
