0d880175a55c5c9101ed086f5b482c11d31c7e1b
NathalieMet <nathalie.metreau@hotmail.fr>
2024-01-31 20:36:35 +0100

    add task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,49 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    let totalStudents = 0;
+    let csStudents = 0;
+    let sweStudents = 0;
+    const listOfCsStudents = [];
+    const listOfSweStudents = [];
+
+    let isFirstLine = true;
+
+    for (const line of lines) {
+      if (isFirstLine) {
+        isFirstLine = false;
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+
+      const fields = line.split(',');
+
+      if (fields.length === 4) {
+        totalStudents += 1;
+        const [firstName, , , field] = fields;
+
+        if (field.trim() === 'CS') {
+          csStudents += 1;
+          listOfCsStudents.push(firstName);
+        }
+
+        if (field.trim() === 'SWE') {
+          sweStudents += 1;
+          listOfSweStudents.push(firstName);
+        }
+      }
+    }
+
+    console.log(`Number of students: ${totalStudents}`);
+    console.log(`Number of students in CS: ${csStudents}. List: ${listOfCsStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents}. List: ${listOfSweStudents.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
