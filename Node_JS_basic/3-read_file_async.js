7978780f7b47d14ed06183681aa55f073d806484
NathalieMet <nathalie.metreau@hotmail.fr>
2024-02-01 17:11:54 +0100

    try correction on t3 to correct t5 later

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,49 +1,53 @@
 const fs = require('fs').promises;
 
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
+function countStudentsAsync(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8')
+      .then((data) => {
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
 
-    let totalStudents = 0;
-    let csStudents = 0;
-    let sweStudents = 0;
-    const listOfCsStudents = [];
-    const listOfSweStudents = [];
+        let totalStudents = 0;
+        let csStudents = 0;
+        let sweStudents = 0;
+        const listOfCsStudents = [];
+        const listOfSweStudents = [];
 
-    let isFirstLine = true;
+        let isFirstLine = true;
 
-    for (const line of lines) {
-      if (isFirstLine) {
-        isFirstLine = false;
-        // eslint-disable-next-line no-continue
-        continue;
-      }
+        for (const line of lines) {
+          if (isFirstLine) {
+            isFirstLine = false;
+            continue;
+          }
 
-      const fields = line.split(',');
+          const fields = line.split(',');
 
-      if (fields.length === 4) {
-        totalStudents += 1;
-        const [firstName, , , field] = fields;
+          if (fields.length === 4) {
+            totalStudents += 1;
+            const [firstName, , , field] = fields;
 
-        if (field.trim() === 'CS') {
-          csStudents += 1;
-          listOfCsStudents.push(firstName);
+            if (field.trim() === 'CS') {
+              csStudents += 1;
+              listOfCsStudents.push(firstName);
+            }
+
+            if (field.trim() === 'SWE') {
+              sweStudents += 1;
+              listOfSweStudents.push(firstName);
+            }
+          }
         }
 
-        if (field.trim() === 'SWE') {
-          sweStudents += 1;
-          listOfSweStudents.push(firstName);
-        }
-      }
-    }
-
-    console.log(`Number of students: ${totalStudents}`);
-    console.log(`Number of students in CS: ${csStudents}. List: ${listOfCsStudents.join(', ')}`);
-    console.log(`Number of students in SWE: ${sweStudents}. List: ${listOfSweStudents.join(', ')}`);
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
+        console.log(`Number of students: ${totalStudents}`);
+        console.log(`Number of students in CS: ${csStudents}. List: ${listOfCsStudents.join(', ')}`);
+        console.log(`Number of students in SWE: ${sweStudents}. List: ${listOfSweStudents.join(', ')}`);
+        resolve(); // Resolve without passing any result as it is not defined in your function
+      })
+      .catch((err) => {
+        console.error('Cannot load the database', err);
+        reject(new Error('Cannot load the database'));
+      });
+  });
 }
 
-module.exports = countStudents;
+module.exports = countStudentsAsync;
