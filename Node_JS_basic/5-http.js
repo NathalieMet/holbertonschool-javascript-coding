f91b054efed64b71d3657338b2ceb97ac1bc091c
NathalieMet <nathalie.metreau@hotmail.fr>
2024-02-02 13:28:14 +0100

    try correction on t5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,7 +1,8 @@
 const http = require('http');
-const path = require('path');
 const fs = require('fs').promises;
 const countStudentsAsync = require('./3-read_file_async');
+
+const database = process.argv[2];
 
 const app = http.createServer(async (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -9,14 +10,12 @@ const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    const filePath = path.resolve(__dirname, 'database.csv');
-
     try {
-      await fs.access(filePath);
+      await fs.access(database);
 
       res.write('This is the list of our students\n');
 
-      countStudentsAsync(filePath)
+      countStudentsAsync(database)
         .then((result) => {
           res.write(`${result.sentence1}\n`);
           res.write(`${result.sentence2}\n`);
