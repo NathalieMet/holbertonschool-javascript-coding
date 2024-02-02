5c5d044c34d6b132b0db7d94cca55b3f70de7810
nathalie <nathalie.metreau@hotmail.fr>
2024-02-02 20:25:52 +0100

    add task 8 almost finished

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,36 @@
+const readDatabase = require('./utils');
+
+class StudentsController {
+	static getAllStudents(request, response) {
+		try {
+			const result = readDatabase(database);
+			response.status(200).send('This is the list of our students\n');
+			response.write(`Number of students in CS: ${result.listOfCsStudents.length}. List: ${result.listOfCsStudents.join(', ')}\n`);
+			response.write(`Number of students in SWE: ${result.listOfSweStudents.length}. List: ${result.listOfSweStudents.join(', ')}\n`);
+			response.end();
+		  } catch (error) {
+			response.status(500).send('Cannot load the database');
+		  }
+		}
+
+		static getAllStudentsByMajor(request, response) {
+			try {
+				const major = request.query.major;
+
+				if (major === 'CS' || major === 'SWE') {
+					const result = readDatabase(database);
+					if (major === 'CS') {
+						response.status(200).send(`List: ${result.listOfCsStudents.join(', ')}\n`);
+					} else {
+						response.status(200).send(`List: ${result.listOfSweStudents.join(', ')}\n`);
+					}
+				} else {
+					response.status(500).send('Major parameter must be CS or SWE');
+				}
+			  } catch (error) {
+				response.status(500).send('Cannot load the database');
+			  }
+			}
+	}
+
+	module.exports = StudentsController;
---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,50 @@
+import fs from 'fs';
+
+function readDatabase(filePath) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(filePath, 'utf8', (err, data) => {
+		  if (err) {
+			reject(new Error(err));
+			return;
+		  }
+		  const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+		  const listOfCsStudents = [];
+		  const listOfSweStudents = [];
+
+		  let isFirstLine = true;
+
+		  for (const line of lines) {
+			if (isFirstLine) {
+			  isFirstLine = false;
+			  // eslint-disable-next-line no-continue
+			  continue;
+			}
+
+			const fields = line.split(',');
+
+			if (fields.length === 4) {
+			  const [firstName, , , field] = fields;
+
+			  if (field.trim() === 'CS') {
+				listOfCsStudents.push(firstName);
+			  }
+
+			  if (field.trim() === 'SWE') {
+				listOfSweStudents.push(firstName);
+			  }
+			}
+		  }
+
+		  const result = {
+			listOfCsStudents,
+			listOfSweStudents
+		  };
+
+		  resolve(result);
+		});
+	  });
+	}
+
+	module.exports = readDatabase;
+
